4e1d9c8d8fa8e9d2ce8a2914342f327880add63e
sneg7777 <71835115+sneg7777@users.noreply.github.com>
2023-06-16 21:43:17 +0900

    Add Dummytower

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameEvent<string> sceneChangedEvent;
 
     private bool isGameScene = false;
+    private bool isPlaceTower = false;
+    private GameObject selectedTower = null;
 
     private void OnEnable()
     {
@@ -21,9 +23,10 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (isGameScene == false) return;
+        //if (isGameScene == false) return;
 
-        Debug.Log("게임 중");
+        //Debug.Log("게임 중");
+        PlaceTower();
     }
 
     private void OnSceneChanged(string sceneName)
@@ -35,6 +38,65 @@ public class GameManager : MonoBehaviour
         else
         {
             isGameScene = false;
+        }
+    }
+
+    private void PlaceTower()
+    {
+        if(!isPlaceTower)
+        {
+            if(Input.GetKeyDown(KeyCode.T))
+            {
+                isPlaceTower = true;
+                GameObject prefab = Resources.Load<GameObject>("TestDummy_01");
+                selectedTower = Instantiate(prefab);
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.CompareTag("TowerTile"))
+                    {
+                        TowerTile towerTile = hit.transform.parent.GetComponent<TowerTile>();
+                        if (towerTile.tileState != TowerTile.TileState.UnUsed)
+                        {
+                            return;
+                        }
+
+                        GameObject prefab = Resources.Load<GameObject>("TestTower_01");
+                        GameObject newTower = Instantiate(prefab);
+                        newTower.transform.position = hit.transform.parent.position;
+                        newTower.transform.Translate(new Vector3(0f, 1f, 0f));
+                        towerTile.tileState = TowerTile.TileState.Used;
+
+                        isPlaceTower = false;
+                        Destroy(selectedTower);
+                        selectedTower = null;
+                    }
+                }
+            }
+            else
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.CompareTag("TowerTile"))
+                    {
+                        selectedTower.transform.position = hit.transform.parent.position;
+                        selectedTower.transform.Translate(new Vector3(0f, 1f, 0f));
+                    }
+                    else
+                    {
+                        selectedTower.transform.position = new Vector3(0f, -100f, 0f);
+                    }
+                }
+            }
         }
     }
 }
